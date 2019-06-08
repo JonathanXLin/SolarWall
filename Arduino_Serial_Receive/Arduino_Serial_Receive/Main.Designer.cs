9bbd70b86c82dc1d5d99bdd28f74dd94972912bf
Jonathan Lin <jlin1999@gmail.com>
2019-06-08 16:27:49 -0400

    Added windows forms app to receive serial data for PC datalogging

---
+++ Arduino_Serial_Receive/Arduino_Serial_Receive/Main.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Arduino_Serial_Receive
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timerSerial = new System.Windows.Forms.Timer(this.components);
+            this.serialPortArduino = new System.IO.Ports.SerialPort(this.components);
+            this.comboBoxPort = new System.Windows.Forms.ComboBox();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBoxPort
+            // 
+            this.comboBoxPort.FormattingEnabled = true;
+            this.comboBoxPort.Location = new System.Drawing.Point(214, 156);
+            this.comboBoxPort.Name = "comboBoxPort";
+            this.comboBoxPort.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxPort.TabIndex = 0;
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(214, 184);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(121, 23);
+            this.buttonConnect.TabIndex = 1;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.ButtonConnect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.comboBoxPort);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timerSerial;
+        private System.IO.Ports.SerialPort serialPortArduino;
+        private System.Windows.Forms.ComboBox comboBoxPort;
+        private System.Windows.Forms.Button buttonConnect;
+    }
+}
+
---
+++ Arduino_Serial_Receive/Arduino_Serial_Receive/Main.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+
+namespace Arduino_Serial_Receive
+{
+    public partial class Main : Form
+    {
+        //Serial connection variables
+        bool isConnectedSerial = false;
+        String[] ports;
+        SerialPort port;
+
+        public Main()
+        {
+            InitializeComponent();
+
+            //Search for serial connections
+            ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+            {
+                comboBoxPort.Items.Add(port);
+                Console.WriteLine(port);
+                if (ports[0] != null)
+                {
+                    comboBoxPort.SelectedItem = ports[0];
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBoxPort.DropDownStyle = ComboBoxStyle.DropDownList;
+        }
+
+        private void ButtonConnect_Click(object sender, EventArgs e)
+        {
+            //Search for serial connections
+            ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+            {
+                comboBoxPort.Items.Add(port);
+                Console.WriteLine(port);
+                if (ports[0] != null)
+                {
+                    comboBoxPort.SelectedItem = ports[0];
+                }
+            }
+
+            MessageBox.Show(serialPortArduino.PortName.ToString());
+        }
+    }
+}
